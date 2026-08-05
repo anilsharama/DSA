4327247c4dd9dfa0fc5c8e43fdfd1aa2047e01c9
anilsharama <215319713+anilsharama@users.noreply.github.com>
2026-08-05 14:16:08 +0530

    Time: 1 ms (100%), Space: 42.9 MB (20.37%) - LeetHub

---
+++ 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfGoodIntegers(int n, int k) {
+        int sum = 0;
+
+        int start = Math.max(0, n - k);
+        int end = n + k;
+
+        for (int i = start; i <= end; i++) {
+            if ((n & i) == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
